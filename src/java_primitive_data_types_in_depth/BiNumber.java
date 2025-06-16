a71281ca8f3ad8d97c19ef5071a0c47510c6f300
Rohan <rohangiramkar227@gmail.com>
2025-06-16 21:51:23 +0530

    add files

---
+++ src/java_primitive_data_types_in_depth/BiNumber.java
@@ -0,0 +1,14 @@
+package java_primitive_data_types_in_depth;
+
+public class BiNumber {
+	private int number1;
+	private int number2;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       this.number1 = number1;
+       this.number2 = number2;
+       
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Rohan
+ *
+ */
+module java_primitive_data_types_in_depth {
+}
